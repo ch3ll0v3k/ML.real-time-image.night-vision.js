936a8d996e3f43aae6246514a7414a92edcf7bc9
ch3ll0v3k <ch3ll0v3k@yandex.com>
2019-07-31 14:22:13 +0200

    descr:[update]

--- public_html/js/tools.js
+++ public_html/js/tools.js
@@ -1,12 +1,17 @@
-function readTextFile(file, callback) {
-  var rawFile = new XMLHttpRequest();
-  rawFile.overrideMimeType("application/json");
-  rawFile.open("GET", file, true);
-  rawFile.onreadystatechange = function() {
-    if (rawFile.readyState === 4 && rawFile.status == "200") {
-      callback(rawFile.responseText);
+async function loadJSONFromURL(file, callback) {
+
+  return new Promise( async(res, rej)=>{
+
+    let rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+      if (rawFile.readyState === 4 && rawFile.status == "200") {
+        res( JSON.parse(rawFile.responseText) );
+      }
     }
-  }
-  rawFile.send(null);
+    rawFile.send(null);
+  });
+
 }
 
